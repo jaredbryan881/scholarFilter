f7331ea6d4c46f0685b02cc01448a48554bc1c39
jaredbryan881 <jared.bryan1998@gmail.com>
2023-03-19 23:42:40 -0400

    Initial commit

---
+++ contentScript.js
@@ -0,0 +1,69 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    var blockList = message.message.split("\n")
+
+    if (typeof message.currentUrl != "undefined") {
+        // Make sure we are not deleting everything!
+        // I didn't combine the conditionals because I want the message passing
+        // to still return as successful
+        if (blockList[0] != ['']) {
+            var currentUrl = message.currentUrl;
+            var arrURLFragments = currentUrl.split("/");
+            var currentDomain = arrURLFragments[2];
+            var currentExtension = arrURLFragments[3];
+
+            // For checking if we are on google scholar
+            var regDomainGoogle = /google/g;
+            var regDomainScholar = /scholar/g;
+
+            // For checking which kind of google scholar page we are on
+            var regExtensionCites = /scholar\?cites/g;
+            var regExtensionAuthor = /citations/g;
+            var regExtensionGeneral = /scholar\?h/g;
+
+            // Make sure we are on google scholar
+            if (regDomainGoogle.test(currentDomain.toLowerCase()) && regDomainScholar.test(currentDomain.toLowerCase())) {
+                var weAreOnSearchPage = $("#gs_bdy ").length;
+                if (weAreOnSearchPage > 0) {
+
+                    if (regExtensionAuthor.test(currentExtension.toLowerCase())) {
+                        // We are on an author's page
+
+                        // Find all text that looks like it could be a journal name
+                        $("div.gs_gray").each(function () {
+                            currentBlock = $(this)
+                            var title = currentBlock.text();
+
+                            // Check if the journal is on the block list
+                            blockList.forEach(function (item, index) {
+                                if (new RegExp(item).test(title)) {
+                                    // Delete this element of the HTML table
+                                    var elementToDelete = currentBlock.parents("td.gsc_a_t").parents("tr.gsc_a_tr");
+                                    if (elementToDelete.length>0){elementToDelete[0].remove()};
+                                }
+                            })
+                        });
+                    } else if (regExtensionGeneral.test(currentExtension.toLowerCase()) || regExtensionCites.test(currentExtension.toLowerCase())) {
+                        // We are on a general search page or a cited-by page
+
+                        // Find all text that looks like it could be a journal name
+                        $("div.gs_a").each(function () {
+                            currentBlock = $(this)
+                            var title = currentBlock.text();
+
+                            // Check if the journal is on the block list
+                            blockList.forEach(function (item, index) {
+                                if (new RegExp(item).test(title)) {
+                                    // Delete this element of the HTML table
+                                    var elementToDelete = currentBlock.parents("div.gs_ri").parents("div.gs_r.gs_or.gs_scl");
+                                    if (elementToDelete.length>0){elementToDelete[0].remove()};
+                                }
+                            })
+                        });
+                    }
+                }
+            }
+        }
+    // Notify the sender that the message was successfully received
+    sendResponse({ received: "OK" });
+    }
+});
---
+++ popup.js
@@ -0,0 +1,81 @@
+(function(window){
+    // Default settings for the size and text in the text box
+    var settings = {
+        // height of the text box when it's empty
+        minHeight : 40,
+        // height of the text box at max capacity
+        maxHeight : 100,
+        // placeholder in the text box
+        placeholder : "Enter block list here"
+    };
+    
+    // DOM is ready
+    document.addEventListener('DOMContentLoaded', function () {
+        var textarea = document.getElementById('textarea'),
+        emptyEvent = new CustomEvent("isEmpty"),
+        overflowEvent = new CustomEvent("isOverflow");
+
+        window.addEventListener('focus', fillTextarea);
+        window.addEventListener('blur', saveTextarea);
+        textarea.removeAttribute('readonly');
+        
+        textarea.addEventListener('isEmpty', function(e){
+            setHeight(settings.minHeight);
+        });
+
+        textarea.addEventListener('isOverflow', function(e){
+            setHeight();
+        });
+    
+        textarea.addEventListener('input', function(e){
+            if(textarea.value.trim() == ''){
+                textarea.dispatchEvent(emptyEvent);
+            }
+            if(textarea.scrollHeight > textarea.offsetHeight){
+                textarea.dispatchEvent(overflowEvent);
+            }
+
+            // Save the text in the text box
+            saveTextarea();
+        });
+
+        // Send message to the content script on button press
+        button.addEventListener('click', sendTextarea);
+
+        function fillTextarea(){
+            chrome.storage.local.get('text', function(result){
+                var val = result.text
+                if(val !== ''){
+                    // focus textarea on first line
+                    textarea.value = '\n'+val;
+                }
+            });
+        }
+
+        function saveTextarea(){
+            chrome.storage.local.set({"text": textarea.value})
+        }
+
+        function sendTextarea(){
+            chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+                var activeTab = tabs[0];
+                chrome.tabs.sendMessage(activeTab.id, {"currentUrl": activeTab.url, 
+                                                       "message": textarea.value});
+            });
+        }
+        
+        function setHeight(value){
+            var val = value || Math.min(textarea.scrollHeight, settings.maxHeight);
+            textarea.style.height = val + 'px';
+        }
+
+        function init(){
+            textarea.setAttribute('placeholder', settings.placeholder);
+            fillTextarea();
+            textarea.focus();
+            setHeight();
+        }
+        
+        init();
+    });
+})(this);
